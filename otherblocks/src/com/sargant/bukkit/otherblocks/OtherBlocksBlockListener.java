f3ac4033bb797d02eb2cdbac2f35f8b3f45f2f14
Robert Sargant <robert.sargant@postgrad.manchester.ac.uk>
2011-02-10 14:40:58 +0000

    Cleaned up whitespace

--- otherblocks/src/com/sargant/bukkit/otherblocks/OtherBlocksBlockListener.java
+++ otherblocks/src/com/sargant/bukkit/otherblocks/OtherBlocksBlockListener.java
@@ -11,12 +11,12 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.block.BlockDamageEvent;
 import org.bukkit.event.block.BlockListener;
 import org.bukkit.inventory.ItemStack;
- 
+
 public class OtherBlocksBlockListener extends BlockListener
 {
 	@SuppressWarnings("unused")
 	private OtherBlocks parent;
-	
+
 	public OtherBlocksBlockListener(OtherBlocks instance)
 	{
 		parent = instance;
@@ -28,15 +28,15 @@ public class OtherBlocksBlockListener extends BlockListener
 		{
 			return;
 		}
-		
+
 		List<Material> goodTools = new ArrayList<Material>();
-		
+
 		goodTools.add(Material.GOLD_AXE);
 		goodTools.add(Material.GOLD_HOE);
 		goodTools.add(Material.GOLD_PICKAXE);
 		goodTools.add(Material.GOLD_SPADE);
 		goodTools.add(Material.GOLD_SWORD);
-		
+
 		// Check that the currently held tool is an appropriate one
 		if(goodTools.contains(event.getPlayer().getItemInHand().getType()) == false)
 		{
@@ -47,33 +47,32 @@ public class OtherBlocksBlockListener extends BlockListener
 		{
 			return;
 		}
-		
+
 		Player p = event.getPlayer();
 		ItemStack t = p.getItemInHand();
 		Block b = event.getBlock();
-		
+
 		Location lx = new Location(b.getWorld(), b.getX(), b.getY(), b.getZ());
-		
+
 		if(b.getType() == Material.SAND && t.getType() == Material.GOLD_SPADE)
 		{
 			event.setCancelled(true);
 			b.setType(Material.AIR);
-		  b.getWorld().dropItemNaturally(lx, new ItemStack(Material.GLOWSTONE_DUST, 4));				
+			b.getWorld().dropItemNaturally(lx, new ItemStack(Material.GLOWSTONE_DUST, 4));
 			t.setDurability((short) (t.getDurability()+1));
 		}
 		else if(b.getType() == Material.COAL_ORE && t.getType() == Material.GOLD_PICKAXE)
 		{
 			event.setCancelled(true);
 			b.setType(Material.AIR);
-		  b.getWorld().dropItemNaturally(lx, new ItemStack(Material.NETHERRACK, 1));				
+			b.getWorld().dropItemNaturally(lx, new ItemStack(Material.NETHERRACK, 1));
 			t.setDurability((short) (t.getDurability()+1));
 		}
-		
+
 		if(t.getDurability() >= 32)
 		{
 			p.setItemInHand(null);
 		}
 	}
 }
-
 
