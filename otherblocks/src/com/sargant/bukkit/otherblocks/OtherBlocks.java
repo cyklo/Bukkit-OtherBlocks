cb67653837cdfed0bdb437983db753f3f523e319
Robert Sargant <robert@sargant.com>
2011-02-10 14:17:35 +0000

    Initial commit

---
+++ otherblocks/src/com/sargant/bukkit/otherblocks/OtherBlocks.java
@@ -0,0 +1,58 @@
+package com.sargant.bukkit.otherblocks;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import org.bukkit.Server;
+import org.bukkit.event.Event;
+import org.bukkit.event.Event.Priority;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginLoader;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.PluginManager;
+
+public class OtherBlocks extends JavaPlugin
+{	
+	public String testval;
+    
+	private final OtherBlocksBlockListener blockListener = new OtherBlocksBlockListener(this); 
+	private final Logger log = Logger.getLogger("Minecraft");
+   
+	public OtherBlocks(PluginLoader pluginLoader, Server instance, PluginDescriptionFile desc, File folder, File plugin, ClassLoader cLoader)
+	{
+		super(pluginLoader, instance, desc, folder, plugin, cLoader);
+
+		folder.mkdirs();
+		File yml = new File(getDataFolder(), "config.yml");
+        
+		if (!yml.exists())
+		{
+			try
+			{
+				yml.createNewFile();
+				log.info("Created an empty file config.yml at " + getDataFolder() +", please edit it!");
+        
+				getConfiguration().setProperty("please", "edit-me");
+				getConfiguration().save();
+				
+			}
+      catch (IOException ex){}
+		}
+		        
+		testval = getConfiguration().getString("please", "edit-me");
+	}
+
+	public void onDisable()
+	{
+		log.info(getDescription().getName() + " " + getDescription().getVersion() + " unloaded.");
+	}
+
+	public void onEnable()
+	{
+		PluginManager pm = getServer().getPluginManager();
+		pm.registerEvent(Event.Type.BLOCK_DAMAGED, blockListener, Priority.Monitor, this);
+
+		log.info(getDescription().getName() + " " + getDescription().getVersion() + " loaded.");
+	}
+}
---
+++ otherblocks/src/com/sargant/bukkit/otherblocks/OtherBlocksBlockListener.java
@@ -0,0 +1,79 @@
+package com.sargant.bukkit.otherblocks;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockDamageLevel;
+import org.bukkit.entity.Player;
+import org.bukkit.event.block.BlockDamageEvent;
+import org.bukkit.event.block.BlockListener;
+import org.bukkit.inventory.ItemStack;
+ 
+public class OtherBlocksBlockListener extends BlockListener
+{
+	@SuppressWarnings("unused")
+	private OtherBlocks parent;
+	
+	public OtherBlocksBlockListener(OtherBlocks instance)
+	{
+		parent = instance;
+	}
+
+	public void onBlockDamage(BlockDamageEvent event)
+	{
+		if (event.isCancelled())
+		{
+			return;
+		}
+		
+		List<Material> goodTools = new ArrayList<Material>();
+		
+		goodTools.add(Material.GOLD_AXE);
+		goodTools.add(Material.GOLD_HOE);
+		goodTools.add(Material.GOLD_PICKAXE);
+		goodTools.add(Material.GOLD_SPADE);
+		goodTools.add(Material.GOLD_SWORD);
+		
+		// Check that the currently held tool is an appropriate one
+		if(goodTools.contains(event.getPlayer().getItemInHand().getType()) == false)
+		{
+			return;
+		}
+
+		if (event.getDamageLevel() != BlockDamageLevel.BROKEN)
+		{
+			return;
+		}
+		
+		Player p = event.getPlayer();
+		ItemStack t = p.getItemInHand();
+		Block b = event.getBlock();
+		
+		Location lx = new Location(b.getWorld(), b.getX(), b.getY(), b.getZ());
+		
+		if(b.getType() == Material.SAND && t.getType() == Material.GOLD_SPADE)
+		{
+			event.setCancelled(true);
+			b.setType(Material.AIR);
+		  b.getWorld().dropItemNaturally(lx, new ItemStack(Material.GLOWSTONE_DUST, 4));				
+			t.setDurability((short) (t.getDurability()+1));
+		}
+		else if(b.getType() == Material.COAL_ORE && t.getType() == Material.GOLD_PICKAXE)
+		{
+			event.setCancelled(true);
+			b.setType(Material.AIR);
+		  b.getWorld().dropItemNaturally(lx, new ItemStack(Material.NETHERRACK, 1));				
+			t.setDurability((short) (t.getDurability()+1));
+		}
+		
+		if(t.getDurability() >= 32)
+		{
+			p.setItemInHand(null);
+		}
+	}
+}
+
+
