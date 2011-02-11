96ed5e22430fe4e5dfd08ad8926392be259982e0
Robert Sargant <robert.sargant@postgrad.manchester.ac.uk>
2011-02-11 14:04:10 +0000

    Moved Container to own class

---
+++ otherblocks/src/com/sargant/bukkit/otherblocks/OtherBlocksContainer.java
@@ -0,0 +1,11 @@
+package com.sargant.bukkit.otherblocks;
+
+import org.bukkit.Material;
+
+public class OtherBlocksContainer
+{
+	public Material original;
+	public Material dropped;
+	public Material tool;
+	public Integer quantity;	
+}
