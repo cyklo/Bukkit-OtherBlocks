95f1cff38bc6300372ff01f5a369c0b9d2f56d43
Robert Sargant <robert@sargant.com>
2011-02-19 13:20:10 +0000

    Can now perform multiple drops per blocks

--- otherblocks/src/com/sargant/bukkit/otherblocks/OtherBlocksBlockListener.java
+++ otherblocks/src/com/sargant/bukkit/otherblocks/OtherBlocksBlockListener.java
@@ -24,17 +24,21 @@ public class OtherBlocksBlockListener extends BlockListener
 			return;
 		}
 		
+		Block target  = event.getBlock();
+		ItemStack tool = event.getPlayer().getItemInHand();
+		Integer maxDamage = 0; 
+		boolean successfulConversion = false;
+		
 		for(OtherBlocksContainer obc : parent.transformList) {
-			
+						
 			// Check held item matches
-			ItemStack tool = event.getPlayer().getItemInHand();
 			if(obc.tool != null && obc.tool != tool.getType()) {
 				continue;
 			}
 			
 			// Check target block matches
-			Block target  = event.getBlock();
-			if(obc.original != target.getType()) {
+			
+			if(obc.original != event.getBlock().getType()) {
 				continue;
 			}
 			
@@ -46,23 +50,32 @@ public class OtherBlocksBlockListener extends BlockListener
 			Location location = new Location(target.getWorld(), target.getX(), target.getY(), target.getZ());
 			
 			// At this point, the tool and the target block match
+			successfulConversion = true;
+			target.getWorld().dropItemNaturally(location, new ItemStack(obc.dropped, obc.quantity, obc.color));
+			maxDamage = (maxDamage < obc.damage) ? obc.damage : maxDamage;
+			
+		}
+		
+		if(successfulConversion) {
+			
+			// Convert the target block
 			event.setCancelled(true);
 			target.setType(Material.AIR);
-			target.getWorld().dropItemNaturally(location, new ItemStack(obc.dropped, obc.quantity, obc.color));
 			
-			// Drop out now if item doesn't have a durability or is a block
+			// Check the tool can take wear and tear
 			if(parent.getFixedMaxDurability(tool.getType()) < 0 || tool.getType().isBlock()) {
-				continue;
+				return;
 			}
 			
-			// Now adjust the durability of the held tool
-			tool.setDurability((short) (tool.getDurability() + obc.damage));
+		  // Now adjust the durability of the held tool
+			tool.setDurability((short) (tool.getDurability() + maxDamage));
 			
 			// Manually check whether the tool has exceed its durability limit
 			if(tool.getDurability() >= parent.getFixedMaxDurability(tool.getType())) {
 				event.getPlayer().setItemInHand(null);
 			}
 		}
+		
 	}
 }
 
