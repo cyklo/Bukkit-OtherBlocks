d17d7eb3819f9659f3cb5e11abc2eed896418101
Robert Sargant <robert@sargant.com>
2011-02-28 12:49:51 +0000

    Added multiple tools per declaration

--- src/com/sargant/bukkit/otherblocks/OtherBlocksBlockListener.java
+++ src/com/sargant/bukkit/otherblocks/OtherBlocksBlockListener.java
@@ -10,46 +10,47 @@ import org.bukkit.event.block.BlockListener;
 import org.bukkit.inventory.ItemStack;
 
 public class OtherBlocksBlockListener extends BlockListener
-{	
+{
 	private OtherBlocks parent;
-	
+
 	public OtherBlocksBlockListener(OtherBlocks instance)
 	{
 		parent = instance;
 	}
-	
+
+	@Override
 	public void onBlockBreak(BlockBreakEvent event)
 	{
-		
+
 		if (event.isCancelled()) {
 			return;
 		}
-		
+
 		Block target  = event.getBlock();
 		ItemStack tool = event.getPlayer().getItemInHand();
-		Integer maxDamage = 0; 
+		Integer maxDamage = 0;
 		boolean successfulConversion = false;
-		
+
 		for(OtherBlocksContainer obc : parent.transformList) {
-						
+
 			// Check held item matches
-			if(obc.tool != null && obc.tool != tool.getType()) {
+			if(!obc.tool.contains(null) && !obc.tool.contains(tool.getType())) {
 				continue;
 			}
-			
+
 			// Check target block matches
-			
+
 			if(obc.original != event.getBlock().getType()) {
 				continue;
 			}
-			
+
 			// Check probability is great than the RNG
 			if(parent.rng.nextDouble() > (obc.chance.doubleValue()/100)){
 				continue;
 			}
-			
+
 			Location location = new Location(target.getWorld(), target.getX(), target.getY(), target.getZ());
-			
+
 			// At this point, the tool and the target block match
 			successfulConversion = true;
 			try {
@@ -65,27 +66,27 @@ public class OtherBlocksBlockListener extends BlockListener
 			}
 			maxDamage = (maxDamage < obc.damage) ? obc.damage : maxDamage;
 		}
-		
+
 		if(successfulConversion) {
-			
+
 			// Convert the target block
 			event.setCancelled(true);
 			target.setType(Material.AIR);
-			
+
 			// Check the tool can take wear and tear
 			if(parent.getFixedMaxDurability(tool.getType()) < 0 || tool.getType().isBlock()) {
 				return;
 			}
-			
-		  // Now adjust the durability of the held tool
+
+			// Now adjust the durability of the held tool
 			tool.setDurability((short) (tool.getDurability() + maxDamage));
-			
+
 			// Manually check whether the tool has exceed its durability limit
 			if(tool.getDurability() >= parent.getFixedMaxDurability(tool.getType())) {
 				event.getPlayer().setItemInHand(null);
 			}
 		}
-		
+
 	}
 }
 
